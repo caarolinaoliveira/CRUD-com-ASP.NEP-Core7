a00a2ca4d919bf239e96ad40f781da83ba532131
caarolinaoliveira <138406827+caarolinaoliveira@users.noreply.github.com>
2024-01-20 14:14:21 -0300

    Add project files.

---
+++ AwesomeDevEvents.API/Persistence/DevEventDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeDevEvents.API.Entities;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+    public class DevEventDbContext
+    {
+
+        public List<DevEvent> DevEvents { get; set; }
+        public DevEventDbContext()
+        {
+            DevEvents = new List<DevEvent>();   
+        }
+    }
+}
